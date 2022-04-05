e56e8c13fb0523781fff270a8bc17db016f7adba
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-04-05 10:05:34 +0200

    scoping lesson

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you are a millenial, ${firstName}.`;
+      console.log(str);
+    }
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1995);
