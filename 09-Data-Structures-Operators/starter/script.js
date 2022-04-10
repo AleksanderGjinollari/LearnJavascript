e72edf5a49e6d9eecd1b0c6d834436a618e12c12
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-04-10 12:30:28 +0200

    coding challenge

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -3,34 +3,36 @@
 // Data needed for a later exercise
 // const flights =
 //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 // Data needed for first part of the section
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-//   order: function (firstIndex, secondIndex) {
-//     return [this.starterMenu[starterIndex], this.mainMenu[firstIndex]];
-//   },
-//   openingHours: {
-//     thu: {
-//       open: 12,
-//       close: 22,
-//     },
-//     fri: {
-//       open: 11,
-//       close: 23,
-//     },
-//     sat: {
-//       open: 0, // Open 24 hours
-//       close: 24,
-//     },
-//   },
-// };
+  //ES6 enhanced object literals
+  openingHours,
 
+  order: function (firstIndex, secondIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[firstIndex]];
+  },
+};
+console.log(restaurant.order);
 // const { name, openingHours, categories } = restaurant;
 // console.log(name, openingHours, categories);
 
@@ -188,3 +190,15 @@ printGoals('gnabry');
 
 team1 < team2 && console.log('Team 1 is more likely to win');
 team2 < team1 && console.log('Team 1 is more likely to win');
+
+let goalNumber = 1;
+for (const scorer of game.scored) {
+  console.log(`Goal ${goalNumber++} ${scorer}`);
+}
+
+let mean = array => {
+  let arraySum = sum(array);
+  return arraySum / array.length;
+};
+
+console.log(mean(12, 23));
