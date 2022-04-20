58e1614b5bcd1148b02946275b3565c97b212987
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-04-20 20:41:31 +0200

    more closures examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -186,18 +186,52 @@
 
 // (() => console.log('This will ALSO never run again'))();
 
-const secureBooking = function () {
-  let passengerCount = 0;
+// const secureBooking = function () {
+//   let passengerCount = 0;
 
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+// console.dir(booker);
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
   };
 };
 
-const booker = secureBooking();
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
 
-booker();
-booker();
-booker();
-console.dir(booker);
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, 1000);
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+boardPassengers(180, 3);
