52476c1e49d63930e8ac8d5d1fb0ae7f8f926188
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-04-20 18:40:19 +0200

    calculate tax function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -111,3 +111,30 @@ bookEW(23, 'StevenWilliams');
 const bookEW23 = book.bind(eurowings, 23);
 bookEW23('Jonas');
 bookEW23('Martha');
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVat = addTax.bind(null, 0.23);
+
+console.log(addVat(100));
+console.log(addVat(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVat2 = addTaxRate(0.23);
+console.log(addVat2(100));
+console.log(addVat2(23));
