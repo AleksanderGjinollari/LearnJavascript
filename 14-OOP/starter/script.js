841e45ae424ef97cd090aed78e531b250878f4a0
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-05-04 18:15:31 +0200

    constructor functions

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -3,11 +3,19 @@
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
+  // Never create a method inside a constructor
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
 };
 
 const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+
 // 1. New {} is created
 // 2. function is called, this = {}
 // 3. {} is linked to prototype
 // 4. function automatically retyrns {}
+
+const matilda = new Person('Matilda', 2001);
+console.log(jonas, matilda);
+console.log(jonas instanceof Person);
