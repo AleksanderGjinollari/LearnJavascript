88ea4f2802c4d3ca0016b4937536f77f64b0090e
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-05-04 19:43:27 +0200

    OOP challenge 1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -43,3 +43,34 @@ Array.prototype.unique = function () {
 console.log(arr.unique());
 
 const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+car1.accelerate();
+car1.accelerate();
+car1.brake();
+car1.brake();
+car1.brake();
+car1.brake();
+car2.accelerate();
+car2.accelerate();
+car2.accelerate();
+car2.brake();
+car2.brake();
+car2.brake();
+car2.brake();
