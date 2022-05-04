5a317a891ec7c56b5b461ffae6dab83389d5a17d
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-05-04 20:07:34 +0200

    Setters and Getters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -79,25 +79,48 @@ car2.brake();
 car2.brake();
 
 class PersonCL {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
   // Methods will be added to .prototype property
   calcAge() {
     console.log(2037 - this.birthYear);
   }
+  greet = function () {
+    console.log(`Hey ${this.firstName}`);
+  };
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCL('Jessica', 1856);
+const jessica = new PersonCL('Jessica Alba', 1856);
 console.log(jessica);
 jessica.calcAge();
 
-PersonCL.prototype.greet = function () {
-  console.log(`Hey ${this.firstName}`);
-};
 jessica.greet();
 
 // 1. Classes are NOT hoisted
 // 2. Classes are first-class citizens
 // 3. Classes are executed in strict mode
+
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
