922df94ba04ad9737dac524fbec50f9eec2ed297
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-05-05 20:25:14 +0200

    Chaining methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -319,10 +319,12 @@ class Account {
 
   deposit(val) {
     this.#movements.push(val);
+    return this;
   }
 
   withdraw(val) {
     this.deposit(-val);
+    return this;
   }
 
   requestLoan(val) {
@@ -330,6 +332,7 @@ class Account {
       this.deposit(val);
       console.log(`Loan approved`);
     }
+    return this;
   }
 
   // 4)Private methods
@@ -352,3 +355,7 @@ acc1.requestLoan(1000);
 console.log(acc1._approveLoan);
 console.log(acc1.getMovements());
 console.log(acc1.pin);
+
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
