255fb4534518fe132dc201dd419b34c976aeaf45
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-05-10 19:57:10 +0200

    challenge 1

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -136,3 +136,15 @@ const getCountryData = function (country) {
 btn.addEventListener('click', function () {
   getCountryData('Albania');
 });
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Problem with geocoding ${response.status}`);
+      return response.json();
+    })
+    .then(data => console.log(`You are in ${data.city}, ${data.country}`))
+    .catch(err => console.log(`${err.message}`));
+};
+whereAmI(52.508, 13.381);
