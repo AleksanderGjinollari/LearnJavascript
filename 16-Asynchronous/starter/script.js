f1a20b38bc1af64fad099462b952de1e7e01812d
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-05-10 20:30:35 +0200

    the event loop in practice

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -152,5 +152,9 @@ console.log('Teststart');
 setTimeout(() => {
   console.log('0 sec timer');
 }, 0);
-Promise.resolve('Resolved promise 1').then(res => console.loc(res));
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 100000000; i++) {}
+  console.log(res);
+});
 console.log('Test end');
