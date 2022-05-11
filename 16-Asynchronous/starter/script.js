ba14206843e8f1bfec0aca92a4e2c1fbd2ffd845
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-05-11 20:39:37 +0200

    Promise.race , Promise.all

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -297,7 +297,7 @@ const whereAmI = async function () {
     throw err;
   }
 };
-console.log('1) Will get location');
+// console.log('1) Will get location');
 // whereAmI()
 //   .then(city => console.log(city))
 //   .catch(err => console.error(`2: ${err.message}`))
@@ -310,7 +310,7 @@ console.log('1) Will get location');
   } catch (err) {
     console.error(`2: ${err.message}`);
   }
-  console.log(`3: Finished getting location`);
+  // console.log(`3: Finished getting location`);
 })();
 // try {
 //   let y = 1;
@@ -320,22 +320,58 @@ console.log('1) Will get location');
 //   alert(err.message);
 // }
 
-const get3Countries = async function (c1, c2, c3) {
-  try {
-    //   const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
-    //   const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
-    //   const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     //   const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+//     //   const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+//     //   const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
 
-    const data = await Promise.all([
-      getJSON(`https://restcountries.com/v2/name/${c1}`),
-      getJSON(`https://restcountries.com/v2/name/${c2}`),
-      getJSON(`https://restcountries.com/v2/name/${c3}`),
-    ]);
-    console.log(data.map(d => d[0].capital));
-    // console.log([data1.capital, data2.capital, data3.capital]);
-  } catch (err) {
-    console.error(err);
-  }
+//     const data = await Promise.all([
+//       getJSON(`https://restcountries.com/v2/name/${c1}`),
+//       getJSON(`https://restcountries.com/v2/name/${c2}`),
+//       getJSON(`https://restcountries.com/v2/name/${c3}`),
+//     ]);
+//     console.log(data.map(d => d[0].capital));
+//     // console.log([data1.capital, data2.capital, data3.capital]);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// get3Countries('albania', 'kosovo', 'germany');
+
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v2/name/italy`),
+    getJSON(`https://restcountries.com/v2/name/albania`),
+    getJSON(`https://restcountries.com/v2/name/china`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(() => {
+      reject(new Error('Request took too long.'));
+    }, sec * 1000);
+  });
 };
 
-get3Countries('albania', 'kosovo', 'germany');
+Promise.race([getJSON(`https://restcountries.com/v2/name/china`), timeout(1)])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+// Promise.any [ES2021]
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another success'),
+]).then(res => console.log(res));
